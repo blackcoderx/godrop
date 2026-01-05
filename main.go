abae9fac734eb64aad9020f6f5b212daa441b9cf
blackocoderx <ziggahemmanuel99@gmail.com>
2026-01-05 20:12:33 +0000

    Feature added - add limit from the terminal

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
+	"strconv"
 	"sync"
 	"time"
 
@@ -14,14 +15,23 @@ import (
 func main() {
 	// --- PART 1: INPUT VALIDATION ---
 	// Need at least 2 args: Program Name, File Name
+
+	downloadLimit := 1 // Limit to 1 download
 	if len(os.Args) < 2 {
 		fmt.Println("Usage: go run . <filename>")
 		return
+	} else if len(os.Args) > 2 {
+		limit := os.Args[2]
+		downloadLimit, _ = strconv.Atoi(limit)
+
 	}
+
 	targetFile := os.Args[1]
 
 	// Check if file exists
-	if _, err := os.Stat(targetFile); err != nil {
+	_, err := os.Stat(targetFile)
+
+	if err != nil {
 		fmt.Printf("Error: File '%s' not found.\n", targetFile)
 		return
 	}
@@ -41,16 +51,17 @@ func main() {
 	fmt.Println("----------------------------------------")
 
 	// --- PART 4: CONCURRENCY STATE ---
-	downloadLimit := 1      // Limit to 1 download
 	currentDownloads := 0   // Counter
 	var mu sync.Mutex       // The "Key" to protect the counter
 	done := make(chan bool) // The "Phone Line" to signal shutdown
 
 	// --- PART 5: HTTP HANDLER ---
+	// Handle root URL
+	// when the user visits / , the func is called
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 
 		// A. LOCK AND CHECK
-		mu.Lock()
+		mu.Lock() // Lock the counter so that no one else can change it while we check ( preventing race conditions )
 		if currentDownloads >= downloadLimit {
 			mu.Unlock()
 			http.Error(w, "Link Expired", http.StatusGone) // 410 Gone
@@ -60,7 +71,7 @@ func main() {
 		// Increment safely
 		currentDownloads++
 		myNum := currentDownloads
-		mu.Unlock()
+		mu.Unlock() // Unlock ASAP and proceed so that the next person can download the file
 
 		// B. SERVE FILE
 		fmt.Printf("[%d/%d] Transferring to %s...\n", myNum, downloadLimit, r.RemoteAddr)
@@ -85,7 +96,8 @@ func main() {
 	// --- PART 6: START SERVER ---
 	// Run in a Goroutine so main() can keep moving
 	go func() {
-		if err := http.ListenAndServe(":"+port, nil); err != nil {
+		err := http.ListenAndServe(":"+port, nil)
+		if err != nil {
 			fmt.Println("Server Error:", err)
 			os.Exit(1)
 		}
@@ -93,6 +105,8 @@ func main() {
 
 	// --- PART 7: WAIT ---
 	// Block here until we receive the signal from the handler
+	// Synchronization without
+
 	<-done
 	fmt.Println("Server stopped. Goodbye!")
 }
