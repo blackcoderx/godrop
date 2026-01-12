5a83365f75665402338937a0fd50a8741b3cf317
blackocoderx <ziggahemmanuel99@gmail.com>
2026-01-12 20:43:47 +0000

    change the app's icon

--- godrop-gui/main.go
+++ godrop-gui/main.go
@@ -24,7 +24,7 @@ func main() {
 		AssetServer: &assetserver.Options{
 			Assets: assets,
 		},
-		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
+		BackgroundColour: &options.RGBA{R: 247, G: 244, B: 243, A: 1},
 		OnStartup:        app.startup,
 		Bind: []interface{}{
 			app,
